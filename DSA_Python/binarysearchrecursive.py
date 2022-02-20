71753851ee3feeff561659fb3f2a0856555f5d55
Sai Venkatesh <saivenkatesh1403@gmail.com>
2022-02-20 23:39:06 +0530

    patterns questions

--- DSA_Python/binarysearchrecursive.py
+++ DSA_Python/binarysearchrecursive.py
@@ -1,17 +1,17 @@
-def binaryseaarch_recursive(A, key, l, r):
+def binarysearch_recursive(A, key, l, r):
     if l > r:
         return -1
     else:
-        mid = (l + r) // 2
+        mid = l + (r - l)// 2
         if key == A[mid]:
             return mid
         elif key < A[mid]:
-            return binaryseaarch_recursive(A,key,l,mid-1)
+            return binarysearch_recursive(A,key,l,mid-1)
         elif key > A[mid]:
-            return binaryseaarch_recursive(A,key,mid+1,r)
+            return binarysearch_recursive(A,key,mid+1,r)
 
 A = [15, 21, 47, 84, 96]
-found = binaryseaarch_recursive(A,17,0,4)
+found = binarysearch_recursive(A,21,0,4)
 print('Result to find binarysearch:', found)
 
 
